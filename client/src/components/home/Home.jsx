8b024959a4f4f48299a9c72e21afd0a1ca4019da
Malek-Ad <malloukaad@gmail.com>
2024-11-23 13:09:23 +0100

    so far

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -73,6 +73,53 @@ const Home = () => {
           <h2>About Us</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
+
+        <div className="fashion-speaks">
+          <div className="fashion-image">
+            <img src="/fashion-statue.jpg" alt="Fashion Statue" />
+          </div>
+          <div className="fashion-content">
+            <h2>Fashion That Speaks</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis
+              molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla
+              accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus.
+              Maecenas eget condimentum velit, sit amet feugiat lectus. Class
+              aptent taciti sociosqu ad litora torquent per conubia nostra, per
+              inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac
+              scelerisque ante pulvinar.
+            </p>
+            <button className="show-more-btn">Show more</button>
+          </div>
+        </div>
+
+        <div className="all-collection">
+          <h2>All Collection</h2>
+          <p>Worlds First Layer 2 Fashion Marketplace</p>
+          
+          <div className="features">
+            <div className="feature-item">
+              <div className="feature-icon">
+                <img src="/dollar-icon.png" alt="No Gas Fees" />
+              </div>
+              <h3>No Gas Fees</h3>
+            </div>
+            
+            <div className="feature-item">
+              <div className="feature-icon">
+                <img src="/nft-icon.png" alt="Carbon Natural NFTs" />
+              </div>
+              <h3>Carbon Natural NFTs</h3>
+            </div>
+            
+            <div className="feature-item">
+              <div className="feature-icon">
+                <img src="/transaction-icon.png" alt="Fast And Easy Transactions" />
+              </div>
+              <h3>Fast And Easy Transactions</h3>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
